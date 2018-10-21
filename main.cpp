0a5c5e8b415e0ed1770789fa3404255d465f2d36
milesbrodie <42948958+milesbrodie@users.noreply.github.com>
2018-10-20 20:36:11 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdlib>
+#include <fstream>
 
 
 class Node
@@ -16,7 +17,7 @@ class LinkedList
 public:
     int length;
     Node* head;
-    
+
 
     LinkedList();
     ~LinkedList();
@@ -35,9 +36,9 @@ LinkedList::~LinkedList(){
 }
 
 Node* LinkedList::Insert(int index, int data){
-    
+
     if (index < 0) return NULL;
-    
+
     int currIndex    =    1;
     Node* currNode    =    head;
     while (currNode && index > currIndex) {
@@ -46,7 +47,7 @@ Node* LinkedList::Insert(int index, int data){
     }
 
     if (index > 0 && currNode == NULL) return NULL;
-    
+
     Node* newNode    =    new    Node;
     newNode->data    =    data;
     if (index == 0) {
@@ -61,15 +62,15 @@ Node* LinkedList::Insert(int index, int data){
     }
     return newNode;
 
-    
-    
+
+
 }
 
 int LinkedList::Delete(int x){
     Node* prevNode = NULL;
     Node* currNode = head;
     int currIndex = 1;
-    
+
     while (currNode && currNode->data != x) {
         prevNode = currNode;
         currNode = currNode->next;
@@ -88,24 +89,54 @@ int LinkedList::Delete(int x){
         }
         return currIndex;
     }
-    
+
     return 0;
 }
 
 void LinkedList::print(){
+    ofstream txt_file;
+    txt_file.open("Miles.Brodie.lList.txt", std::ios_base::app);
+
     Node* head = this->head;
     int i = 1;
+    txt_file << "List Length: " << this->length << endl;
     while(head){
-        std::cout << i << ": " << head->data<<endl;
+        txt_file << head->data << " ";
         head = head->next;
         i++;
     }
+
+    txt_file << "\n\n";
+
+    txt_file.close();
 }
+
+/*
+template<class T>
+void LinearList<T>::Output()const
+{
+    ofstream txt_file;
+    txt_file.open("Miles.Brodie.aList.txt", std::ios_base::app);
+
+    if(isEmpty())
+        txt_file <<"list is empty\n";
+    else
+        for(int i=0; i<length; i++){
+            txt_file << element[i] << " ";
+        }
+    txt_file << "\n";
+
+    txt_file.close();
+}
+
+*/
+
+
 
 int main(int argc, char const *argv[])
 {
     LinkedList* list = new LinkedList();
- 
+
     //list->add(0,rand() % 10);
     list->Insert(0, 11);
     list->Insert(1, 10);
@@ -118,24 +149,23 @@ int main(int argc, char const *argv[])
     list->Insert(8, 3);
     list->Insert(9, 2);
     list->Insert(10, 1);
-    
-    list->print();
+
     std::cout << "List Length: " << list->length << std::endl;
-    
-    cout << endl;
-    
+    list->print();
+
+    cout << endl << endl;
+
     list->Delete(5);
-    list->print();
     std::cout << "List Length: " << list->length << std::endl;
-    
-    cout << endl;
-    
+    list->print();
+
+    cout << endl << endl;
+
     list->Insert(8, 100);
-    list->print();
     std::cout << "List Length: " << list->length << std::endl;
-    
-    //delete list;
-    
-    
+    list->print();
+
+    cout << endl << endl;
+
     return 0;
 }
